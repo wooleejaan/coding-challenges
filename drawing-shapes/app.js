29e6784ff602e05d1441e30ef80f363ebec383aa
yongwoo <wooleejaan@gmail.com>
2024-05-12 17:26:16 +0900

    fix: Fixed file

--- drawing-shapes/app.js
+++ drawing-shapes/app.js
@@ -1,55 +1,22 @@
-// 슬라이더의 동작을 관리하는 클래스
-class DancingSlider {
-  constructor(offset) {
-    this.slider = createSlider(0, 255, 50); // 슬라이더 생성
-    this.offset = offset; // 각 슬라이더의 offset 초기화
-  }
-
-  // 슬라이더를 움직이는 메소드
-  dance(angle) {
-    let x = map(sin(angle + this.offset), -1, 1, 0, 255);
-    this.slider.value(x);
-  }
-}
-
-var sliders = [];
-var angle = 0;
-
 function setup() {
-  noCanvas();
-  for (let i = 0; i < 50; i++) {
-    sliders.push(new DancingSlider(i * 0.025)); // 각 슬라이더에 대해 새로운 인스턴스 생성 및 offset 설정
-  }
+  createCanvas(400, 400);
 }
 
 function draw() {
-  for (let slider of sliders) {
-    slider.dance(angle);
-  }
-  angle += 0.04; // angle의 단위는 radian이다.
+  background(220, 0, 200);
+
+  // 출처 : https://www.youtube.com/watch?v=c3TeLi6Ns1E&list=PLRqwX-V7Uu6Zy51Q-x9tMWIv9cueOFTFA&index=4
+  // a Cartesian coordinate system은 2차원 공간을 설명하는 방법.
+  // 2D 캔버스는 데카르트 좌표계를 따르지만, 기본값(좌표계와 픽셀 및 컴퓨터 그래픽스에서 정의하는 표준 방식)이 약간 다르다. 왼쪽 상단이 (0,0)으로 사용된다.
+
+  // rect(x, y, width, height)
+  // x,y가 직사각형의 왼쪽 상단의 위치를 결정한다.
+  // 때로는, 정사각형의 중심을 설정한 뒤에, 그림을 그려나가는 게 훨씬 편리할 때가 있다. => rectMode(CENTER)
+  rect(100, 50, 25, 75, 80);
+
+  line(0, 0, 400, 400);
+
+  // 색상은 컴퓨터에서 어떻게 동작할까?
+  // 디지털 색상을 만드는 방법은 Red, Green, Blue를 어느 정도 혼합하는 것. 마치 빛을 섞는 것과 같다.
+  // 모든 색상을 더 많이 추가할수록 더 밝아진다.
 }
-
-// var sliders = [];
-// var angle = 0;
-
-// function setup() {
-//   noCanvas();
-//   for (var i = 0; i < 50; i++) {
-//     sliders[i] = createSlider(0, 255, 50);
-//   }
-// }
-
-// function draw() {
-//   // 약간의 offset을 부여하면 각각의 slider의 움직임이 달라진다.
-//   var offset = 0;
-//   // sign wave로 랜덤한 움직임을 만들어낸다.
-//   // sign 함수는 y축은 1과 -1 사이를 움직인다. x축은 시간에 따라 움직이는 각도이다.
-//   for (var i = 0; i < sliders.length; i++) {
-//     var x = map(sin(angle + offset), -1, 1, 0, 255); // 1과 -1 사이의 값을 0~255로 맵핑한다.
-//     sliders[i].value(x);
-//     // background(sliders[i].value(x)); // slider의 값으로 배경색을 싱크 맞춘다.
-//     offset += 0.025;
-//   }
-
-//   angle += 0.04; // angle의 단위는 radian이다.
-// }
